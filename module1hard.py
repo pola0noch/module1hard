9ddb19f9e71f329d1ee43472f8b65f920a44109f
pola0noch <klayriddell@gmail.com>
2024-07-08 23:42:21 +0300

    Update module1hard.py

--- module1hard.py
+++ module1hard.py
@@ -4,6 +4,6 @@ students = list(sorted(students))
 grades = [sum(grades[0])/len(grades[0]), sum(grades[1])/len(grades[1]),
           sum(grades[2])/len(grades[2]), sum(grades[3])/len(grades[3]), sum(grades[4])/len(grades[4])]
 
-students_average_score = {students[0] : grades[0],students[1] : grades[1], students[2] : grades[2],
+students_average_score = {students[0] : grades[0], students[1] : grades[1], students[2] : grades[2],
                           students[3] : grades[3], students[4] : grades [4]}
 print(students_average_score)
